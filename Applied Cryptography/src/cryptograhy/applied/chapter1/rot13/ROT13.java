917309256403eb6c508834e9dfb12c84e3c1d24f
zach <zach@localhost>
2020-01-23 19:11:52 -0700

    Added java.util.logging

--- Applied Cryptography/src/cryptograhy/applied/chapter1/rot13/ROT13.java
+++ Applied Cryptography/src/cryptograhy/applied/chapter1/rot13/ROT13.java
@@ -1,58 +1,66 @@
+/* 
+Update JVM default logging properties file at this location
+/Library/Java/JavaVirtualMachines/jdk-13.jdk/Contents/Home/conf/logging.properties
+*/
+
 package cryptograhy.applied.chapter1.rot13;
+
+import java.util.logging.Level; 
+import java.util.logging.Logger; 
+import java.util.logging.*; 
 
 public class ROT13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		boolean debug = false;
+		LogManager logger = LogManager.getLogManager();
+		Logger log = logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+		
 		int i = 0;
 		
-		if(debug) {
-			System.out.printf("Arguments length: %d \n", args.length);
-		}
+		log.log(Level.INFO, String.format("Arguments length: %d \n", args.length));
+
 
 		while (i < args.length) {
 			String c = args[i];
-			if(debug) {
-				System.out.printf("String : %s\n", c);
-			}
+			
+			log.log(Level.INFO, String.format("String : %s\n", c));
+			
 			
 			int l = args[i].length();
-			if(debug) {
-				System.out.printf("Length of string: %d\n", l);
-			}
+			
+			log.log(Level.INFO, String.format("Length of string: %d\n", l));
+			
 			int t = 0;
 			for (int x = 0; x < l; x++) {
-				if(debug) {
-					System.out.printf("Working on character : %d\n", x);
-				}
+				
+				log.log(Level.INFO, String.format("Working on character : %d\n", x));
 				
 				if (c.charAt(x) >= 'a' && c.charAt(x) <= 'm') {
 					t = (c.charAt(x)+13);
-					if(debug) {
-						System.out.printf("+13 : %s -> %s\n", c.charAt(x), (char)t);
-					}
+					
+					log.log(Level.INFO, String.format("+13 : %s -> %s\n", c.charAt(x), t));
+					
 					
 				} else if (c.charAt(x) >= 'n' && c.charAt(x) <= 'z') {
 					t = (c.charAt(x)-13);
-					if(debug) {
-						System.out.printf("-13 : %s -> %s\n", c.charAt(x), (char)t);
-					}
+					
+					log.log(Level.INFO, String.format("-13 : %s -> %s\n", c.charAt(x), t));
+					
 					
 				} else if (c.charAt(x) >= 'A' && c.charAt(x) <= 'M') {
 					t = (c.charAt(x)+13);
-					if(debug) {
-						System.out.printf("+13 : %s -> %s\n", c.charAt(x), (char)t);
-					}
+					
+					log.log(Level.INFO, String.format("+13 : %s -> %s\n", c.charAt(x), t));
+				
 				} else if (c.charAt(x) >= 'N' && c.charAt(x) <= 'Z') {
 					t = (c.charAt(x)-13);
-					if(debug) {
-						System.out.printf("-13 : %s -> %s\n", c.charAt(x), (char)t);
-					}
+					
+					log.log(Level.INFO, String.format("-13 : %s -> %s\n", c.charAt(x), t));
+					
 				} else {
-					if(debug) {
-						System.out.printf("Sorry unknown letter: %s", c.charAt(x));
-					}
+					
+					log.log(Level.INFO, String.format("Sorry unknown letter: %s", c.charAt(x)));
+					
 				}
 				System.out.printf("%s", (char)t);
 			}
--- Applied Cryptography/src/module-info.java
+++ Applied Cryptography/src/module-info.java
@@ -1,2 +1,3 @@
 module applied_cryptography {
+	requires java.logging;
 }
